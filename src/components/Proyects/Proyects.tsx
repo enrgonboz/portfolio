56b9be2dc65d23cf3c445088f63a8f8765439226
Enrique <enrgonboz@alum.us.es>
2023-04-12 18:10:45 +0200

    static asset handling vite

--- src/components/Proyects/Proyects.tsx
+++ src/components/Proyects/Proyects.tsx
@@ -3,49 +3,56 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import { CardMedia } from '@mui/material'
+import cgt from '../../static/images/cgt.png'
+import todo from '../../static/images/todo.png'
+import energia from '../../static/images/energia.jpg'
+import peces from '../../static/images/peces.jpg'
+import camping from '../../static/images/camping.jpg'
+import hormigon from '../../static/images/hormigon.jpg'
+import tunel from '../../static/images/tunel.jpg'
 
 const ProyectsList = [
   {
     id: 1,
     title: 'Google Translate usando ChatGPT',
     description: 'Clon de Google Translate usando React y la api de ChatGPT para traducir textos identificando el idioma de origen a Inglés, Alemán o Castellano',
-    photo: 'src/static/images/cgt.png'
+    photo: cgt
   },
   {
     id: 2,
     title: 'TodoList',
     description: 'Todo List con React y TypeScript',
-    photo: 'src/static/images/todo.png'
+    photo: todo
   },
   {
     id: 3,
     title: 'Gestor de ahorro de energía',
     description: 'Gestor de ahorro de energía usando React y TypeScript, con un diseño responsive, para una gran empresa de energía',
-    photo: 'src/static/images/energia.jpg'
+    photo: energia
   },
   {
     id: 4,
     title: 'Gestor de sensores de temperatura',
     description: 'Gestor de sensores de temperatura en piscifactorias usando React y TypeScript',
-    photo: 'src/static/images/peces.jpg'
+    photo: peces
   },
   {
     id: 5,
     title: 'Web Alquiler de Campings',
     description: 'Gestor de Alquiler de tiendas en campings usando React',
-    photo: 'src/static/images/camping.jpg'
+    photo: camping
   },
   {
     id: 6,
     title: 'Gestor de Albaranes en Obra',
     description: 'Gestor de Albaranes de pruebas en hormigon de Obra usando NodeJs y TypeScript',
-    photo: 'src/static/images/hormigon.jpg'
+    photo: hormigon
   },
   {
     id: 7,
     title: 'Gestor de datos en la construcción de túneles',
     description: 'Gestor de datos en la construcción de túneles usando NodeJs y TypeScript',
-    photo: 'src/static/images/tunel.jpg'
+    photo: tunel
   }
 ]
 
